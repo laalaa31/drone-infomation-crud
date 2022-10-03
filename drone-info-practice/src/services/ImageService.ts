befae591ec039163de0cff034cfa08d42d726aa1
laalaa31 <jseoh4298@gmail.com>
2022-10-03 22:25:14 +0900

    [FEAT] createImages service 구현

--- drone-info-practice/src/services/ImageService.ts
+++ drone-info-practice/src/services/ImageService.ts
@@ -20,6 +20,33 @@ const createImage = async (
   return data;
 };
 
+const createImages = async (
+  imageList: {
+    location: string;
+    originalname: string;
+  }[]
+): Promise<ImageResponseDto[]> => {
+  const data = await Promise.all(
+    imageList.map(async (singleImage) => {
+      const image = new Image({
+        link: singleImage.location,
+        imageName: singleImage.originalname,
+      });
+
+      await image.save();
+
+      return {
+        _id: image._id,
+        imageName: image.imageName,
+        link: image.link,
+      };
+    })
+  );
+
+  return data;
+};
+
 export default {
   createImage,
+  createImages,
 };
